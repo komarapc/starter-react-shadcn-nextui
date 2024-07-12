712a77ca6fee02cbd9dfb961271fd24fd331f8db
izmi.komar <komar.izmi@gmail.com>
2024-07-13 03:57:08 +0700

    feat: Remove Tic Tac Toe game page and route

--- src/routes/tic-tac-toe.lazy.tsx
+++
@@ -1,6 +0,0 @@
-import TicTacToe from "@/pages/TicTacToe";
-import { createLazyFileRoute } from "@tanstack/react-router";
-
-export const Route = createLazyFileRoute("/tic-tac-toe")({
-  component: TicTacToe,
-});
